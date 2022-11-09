2659ece1beda856a5213a0c1839a586108e53ad2
Ingabire <imimaculee@gmail.com>
2022-11-09 15:52:16 +0200

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import "dotenv/config";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import routes from "./routes/index"
+import routes from "./routes/index";
+import cors from "cors";
 
 const app = express();
-
+app.use (cors());
 app.use(bodyParser.json());
 app.use("/v1/shop",routes);
 const dbUrl = process.env.DATABASE_URL;
